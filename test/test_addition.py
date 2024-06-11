ffb29e66107d604868b612c53dbd30db218e8a34
Ashok R <149650688+Ashokkumaran17@users.noreply.github.com>
2024-06-11 11:10:16 +0530

    Update test_addition.py

--- test/test_addition.py
+++ test/test_addition.py
@@ -7,9 +7,9 @@ def test_add():
     assert result == 8
   
 def test_sub():
-    result = add(13, 5)
+    result = subtract(13, 5)
     assert result == 8
   
 def test_mul():
-    result = add(10, 5)
+    result = multiply(10, 5)
     assert result == 50
