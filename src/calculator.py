8234786e79e06fdf0139142caddc086ae1c95824
Ashok R <149650688+Ashokkumaran17@users.noreply.github.com>
2024-06-11 13:37:09 +0530

    Update calculator.py
    fix added

--- src/calculator.py
+++ src/calculator.py
@@ -10,3 +10,6 @@ def multiply(a, b):
 def divide(a, b):
     checkInputs(a, b)
     return a / b
+def checkInputs(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("Inputs must be either int or float!")
