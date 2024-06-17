1fcb224833f2f4397be3630078d7e2a5cc93d4d2
Ashok R <149650688+Ashokkumaran17@users.noreply.github.com>
2024-06-17 15:13:14 +0530

    Updated test_calc.py

--- test/test_calc.py
+++ test/test_calc.py
@@ -2,14 +2,14 @@ from src.calculator import add,subtract,multiply
 import pytest
 
 
-def test_add():
+def test_add_1():
     result = add(3, 5)
     assert result == 8
   
-def test_sub():
+def test_sub_2():
     result = subtract(13, 5)
     assert result == 8
   
-def test_mul():
+def test_mul_3():
     result = multiply(10, 5)
     assert result == 50
